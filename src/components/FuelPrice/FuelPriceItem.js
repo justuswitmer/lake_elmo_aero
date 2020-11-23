5a69ae2b4d2032568a2b90b71fed94cf552a735e
justuswitmer <67343580+justuswitmer@users.noreply.github.com>
2020-11-23 15:09:24 -0600

    Initialize feature_sms

---
+++ src/components/FuelPrice/FuelPriceItem.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { connect, useDispatch, useSelector } from "react-redux";
+import mapStoreToProps from '../../redux/mapStoreToProps';
+
+
+function FuelPriceItem(props) {
+  const [fuelPrice, setFuelPrice] = useState(props.price.pricePerGal);
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    console.log('here is my price ready to go to the db', fuelPrice);
+    dispatch({
+      type: "UPDATE_PRICE",
+      url: `price/${props.price.id}`,
+      payload: {newPrice: fuelPrice}
+    });
+  }
+
+  return (
+    <section className="m-1">
+      <p className="lead text-bold">{props.price.type}</p>
+      <p className="sm">Current Price: {props.price.pricePerGal}/gal</p>
+      <input
+        className="sm"
+        type='money'
+        placeholder='new price/gal'
+        value={fuelPrice}
+        onChange={(event) => setFuelPrice(event.target.value)}
+      />
+      <button
+        className="sm"
+        onClick={handleSubmit}
+      >Change {props.price.type} Price
+                  </button>
+    </section>
+  );
+}
+
+export default connect(mapStoreToProps)(FuelPriceItem);
